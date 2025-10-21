e907e4756b1b90926a71ff41801f76dfa6683219
livivip <palazduygu355@gmail.com>
2025-10-21 00:48:34 -0700

    Update env03.js

--- environ/env03.js
+++ environ/env03.js
@@ -34,3 +34,37 @@ $("#happyButton").click( function() {
 
 })
 
+$("#experiment").append("<p>here as the human, you are responsible for choosing the fate of chopper, pls pick a button to press ... </p>" );
+$("#experiment").css({
+    "color": "white",
+    "background-color": "purple",
+    "font-family": "monospace",
+    "text-align": "center",
+    "font-size": "15"
+})
+
+$("#bottomEx").append("<p>you either press a button or the whole world explodes ...</p>" );
+$("#bottomEx").css({
+    "color": "white",
+    "background-color": "purple",
+    "font-family": "monospace",
+    "text-align": "center",
+    "font-size": "15"
+})
+
+let chopper = {
+    species: "reindeer",
+    age: 15,
+    birthday: "december 24, 1507",
+    sign: "capricorn",
+    cuteness: 100000,
+}
+
+$("#aboutChopper").append("<p>let's learn more about CHOPPER: he is " + chopper.age + " years old and born on " + chopper.birthday+ ", making him a "+ chopper.sign +". not to mention he's a " + chopper.species + " !!! oh and his cuteness level is always at" + chopper.cuteness +" ^-^ </p>" );
+$("#aboutChopper").css({
+    "color": "white",
+    "background-color": "purple",
+    "font-family": "monospace",
+    "text-align": "center",
+    "font-size": "15"
+})
